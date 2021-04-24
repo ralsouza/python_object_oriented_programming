2acb14dafc5bad0a6c5a3489cdbfda4091562475
Rafael Lima <ls.rafael@icloud.com>
2021-04-24 20:39:55 -0300

    Inharitance and subclasses.

---
+++ t04_inheritance_creating_subclasses.py
@@ -0,0 +1,73 @@
+class Employee:
+    """My employee class."""
+
+    # Instance variable.
+    raise_amt = 1.04
+
+    # This is the constructor.
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + "." + last + "@e-mail.com"
+        self.pay = pay
+
+    def full_name(self):
+        return "{} {}".format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+
+class Developer(Employee):
+    raise_amt = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print("-->", emp.full_name())
+
+
+dev1 = Developer(first="Rafael", last="Lima", pay=50000, prog_lang="Python")
+dev2 = Developer(first="Test", last="User", pay=700000, prog_lang="Java")
+
+mgr_1 = Manager("Sue", "Smith", 120000, [dev1])
+
+print(mgr_1.email)
+mgr_1.print_emps()
+
+print("---- Adding dev2 ----")
+
+mgr_1.add_emp(dev2)
+mgr_1.print_emps()
+
+print("---- Removing dev1 ----")
+mgr_1.remove_emp(dev1)
+mgr_1.print_emps()
+
+# print(dev1.email)
+# print(dev1.prog_lang)
+
+# print(dev1.pay)
+# dev1.apply_raise()
+# print(dev1.pay)
